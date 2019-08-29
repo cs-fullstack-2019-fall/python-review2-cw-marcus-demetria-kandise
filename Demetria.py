b1930f8580970c94766036d1dfa43b036e833f5a
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-08-29 10:35:10 -0500

    last

--- Demetria.py
+++ Demetria.py
@@ -35,5 +35,6 @@ def travelList ():
     tlist= ['pack', 'flight','hotel','shop']
 
 userInput=""
-while userInput = (1):
+while userInput == 1:
     userInput= int(input("Please make a selection "))
+travelList()
